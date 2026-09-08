95828f934b40609437ccbbc8ef86bfd53cc6d132
johnshandUX <johnshand.ux@gmail.com>
2026-09-08 21:44:26 +0100

    Add package publishing and TypeScript declarations

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve("src/index.ts"),
